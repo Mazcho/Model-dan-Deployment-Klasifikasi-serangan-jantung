fc92040f4dc40427e71f92abc09863895fcd3b34
MazCho <77985996+Mazcho@users.noreply.github.com>
2023-09-18 02:24:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import numpy as np
 import pickle
 import matplotlib.pyplot as plt
 from sklearn import preprocessing
-from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.neighbors import KNeighborsClassifier
 
 
 #call Model
-model = pickle.load(open('modelgbr.pkl', 'rb'))
+model = pickle.load(open('modelknn.pkl', 'rb'))
 
 #call data
 #call data
@@ -59,50 +59,50 @@ with tab2:
     col1,col2 = st.columns(2)
     with col1 :
         #membuat inputan umur
-        umur = st.number_input("Masukan umur: ", value=0)
+        age = st.number_input("Masukan umur: ", value=0, key="age")
     with col1:
         #membuat gender option
         gender_option={
             1 : "Laki-Laki",
             2 : "Perempuan"
         }
-        jenis_kelamin = st.selectbox("Pilih Jenis Kelamin",options=list(gender_option.keys()), format_func=lambda x: gender_option[x])
-        selected_value = gender_option[jenis_kelamin]
+        gender = st.selectbox("Pilih Jenis Kelamin",options=list(gender_option.keys()), format_func=lambda x: gender_option[x])
+        # gender = gender_select[gender_option]
     with col1:
-        glukosa = st.number_input("Masukan Kadar Glukosa : ",value=0)
-        kcm = st.number_input("Masukan Kreatin Serum (KCM) : ",value=0.0, step = 0.1)
+        glucose = st.number_input("Masukan Kadar Glukosa : ",value=0, key="glucose")
+        kcm = st.number_input("Masukan Kreatin Serum (KCM) : ",value=0.0, step = 0.1, key="kcm")
     with col2:
-        impulse = st.number_input("Masukan impulse: ", value=0)
-        tekanan_atas = st.number_input("Masukan Tekanan darah bagian atas : ",value=0)
-        tekanan_bawah = st.number_input("Masukan Tekanan darah bagian bawah : ",value=0)
-        troponin = st.number_input("Masukan troponin : ",min_value=0.00, step = 0.001)
+        impulse = st.number_input("Masukan impulse: ", value=0, key="impluse")
+        pressurehight = st.number_input("Masukan Tekanan darah bagian atas : ",value=0, key="pressurehight")
+        pressurelow = st.number_input("Masukan Tekanan darah bagian bawah : ",value=0, key="pressurelow")
+        troponin = st.number_input("Masukan troponin : ",min_value=0.00, step = 0.001, key="troponin")
 
     #prediksi ML
     prediksi_serangan_jantung = ''
 
     #tombol prediksi
     if st.button("Prediksi"):
-        prediksi_serangan_jantung = model.predict([[umur,jenis_kelamin,impulse,tekanan_atas,tekanan_bawah,glukosa,kcm,troponin]])
+        prediksi_serangan_jantung = model.predict([[age,gender,impulse,pressurehight,pressurelow,glucose,kcm,troponin]])
         if prediksi_serangan_jantung == 1:
             col4,col5 = st.columns(2)
             with col4:
                 st.image("heartatt.png",width=400)
             with col5:
                 st.write("Hasil")
-                if tekanan_atas <=139 and tekanan_bawah<90:
+                if pressurehight <=139 and pressurelow<90:
                     st.write("Tekanan darah Normal")
-                elif tekanan_atas<=160 and tekanan_bawah<=90:
+                elif pressurehight<=160 and pressurelow<=90:
                     st.write("Anda memiliki hipertensi ringan")
-                elif tekanan_atas<=180 and tekanan_bawah<=110:
+                elif pressurehight<=180 and pressurelow<=110:
                     st.write("Anda memiliki hipertensi sedang")
-                elif tekanan_atas<=200 and tekanan_bawah<=120:
+                elif pressurehight<=200 and pressurelow<=120:
                     st.write("Anda memiliki hipertensi berat")
-                elif tekanan_atas>200 and tekanan_bawah>120:
+                elif pressurehight>200 and pressurelow>120:
                     st.write("Anda memiliki hipertensi Maligna")
-                if glukosa >100:
-                    st.write("Glukosa darah tidak normal (Prediabetes)")
-                elif glukosa>125:
-                    st.write("Glukosa darah tinggi (Diabetes)")  
+                if glucose >100:
+                    st.write("glucose darah tidak normal (Prediabetes)")
+                elif glucose>125:
+                    st.write("glucose darah tinggi (Diabetes)")  
                 st.error('Memiliki Resiko Serangan Jantung')
         else:
             col7,col8 = st.columns(2)
@@ -110,23 +110,22 @@ with tab2:
                 st.image("jantung.png",width=300)
             with col8:
                 st.write("Hasil")
-                if tekanan_atas <=139 and tekanan_bawah<90:
+                if pressurehight <=139 and pressurelow<90:
                     st.write("Tekanan darah Normal")
-                elif tekanan_atas<=160 and tekanan_bawah<=90:
+                elif pressurehight<=160 and pressurelow<=90:
                     st.write("Anda memiliki hipertensi ringan")
-                elif tekanan_atas<=180 and tekanan_bawah<=110:
+                elif pressurehight<=180 and pressurelow<=110:
                     st.write("Anda memiliki hipertensi sedang")
-                elif tekanan_atas<=200 and tekanan_bawah<=120:
+                elif pressurehight<=200 and pressurelow<=120:
                     st.write("Anda memiliki hipertensi berat")
-                elif tekanan_atas>200 and tekanan_bawah>120:
+                elif pressurehight>200 and pressurelow>120:
                     st.write("Anda memiliki hipertensi Maligna")
-                if glukosa >100:
-                    st.write("Glukosa darah tidak normal (Prediabetes)")
-                    st.write('Sebaiknya perhatikan konsumsi glukosa anda agar tetap mejaga kesehatan jantung dengan baik')
-                elif glukosa>125:
-                    st.write("Glukosa darah tinggi (Diabetes)")
+                if glucose >100:
+                    st.write("glucose darah tidak normal (Prediabetes)")
+                    st.write('Sebaiknya perhatikan konsumsi glucose anda agar tetap mejaga kesehatan jantung dengan baik')
+                elif glucose>125:
+                    st.write("glucose darah tinggi (Diabetes)")
                 st.success('Tidak Memiliki Resiko Serangan Jantung')
-
     
 with tab3:
     st.header("Tentang Dataset")
