4cdf18d0f025f2873823271d8fa004eba473c400
MazCho <77985996+Mazcho@users.noreply.github.com>
2023-09-17 19:00:10 +0700

    Update main2.py
    Delet numpy and pandas

--- main2.py
+++ main2.py
@@ -1,10 +1,6 @@
 #Import Library
 import streamlit as st
-import pandas as pd
-import numpy as np
-import plotly.express as px
 import pickle
-import matplotlib.pyplot as plt
 from sklearn import preprocessing
 from sklearn.ensemble import GradientBoostingClassifier
 
